8dd80326c71f0d906010dbf5f6f180b0197fd116
Zainab Abdul Majeed <zj469909@gmail.com>
2024-04-29 12:44:35 +0500

    Final Assignment

---
+++ index.ts
@@ -0,0 +1,36 @@
+//Personal Message:Store a person’s name in a variable,and print a message to that person.
+let Name ="Mirha";
+console.log(`Hello,I am ${Name}.`);
+//Name Cases:Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+//Famous Quote:Find a quote from a famous person you admire. Print the quote and the name of its author.
+let famousquote="A winner is a dreamer Who never gives up";
+console.log(`Nelson Mandela once said:"${famousquote}".`);
+//Famous Quote 2:Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.Then compose your message and store it in a new variable called message.Print your message.
+let famous_person="Albert Einstein";
+let Message="Failure is success in progress";
+console.log(`${famous_person} once said:"${Message}".`);
+//Stripping Names:Store a person’s name,and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+//Write addition,subtraction,multiplication,and division operations that each result in the number 8.
+let a=2;
+let b=6;
+console.log(`addition=${a+b}`);
+let c=10;
+let d=2;
+console.log(`subtraction=${c-d}`);
+let e=2;
+let f=4;
+console.log(`multiplication=${e*f}`);
+let g=16;
+let h=2;
+console.log(`division=${g/h}`);
+//Favorite Number:Store your favorite number in a variable.Then,using that variable,create a message that reveals your favorite number.Print that message.
+let FavoriteNumber=6;
+let message=`My favorite number is :${FavoriteNumber}.`
+console.log(message);
+//Adding Comments:Choose two of the programs you’ve written,and add at least one comment to each.If you don’t have anything specific to write because your programs are too simple at this point,just add your name and the current date at the top of each program file.Then write one sentence describing what the program does.
+//Store the names of a few of your friends in a array called names.Print each person’s name by accessing each element in the list,one at a time.
+let MyFriends=["Ayesha","Noor","Fatima"];
+console.log(MyFriends);
+console.log(MyFriends[0]);
+console.log(MyFriends[1]);
+console.log(MyFriends[2]);
